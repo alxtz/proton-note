41a4f54efdb757e2dc25e196b5bfc5a4b8d345f7
Alex Tzeng <alxtz.tw@gmail.com>
2020-08-25 07:59:26 +0800

    Updates

---
+++ precache-manifest.efd76c4466cf07b1ac0674a12da282fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4eea61109b5b5a9047a4352ac7caf7",
+    "url": "/proton-note/index.html"
+  },
+  {
+    "revision": "8d41d322dd648297e007",
+    "url": "/proton-note/static/css/main.d40a11f4.chunk.css"
+  },
+  {
+    "revision": "5bad8693ad2e17b4a856",
+    "url": "/proton-note/static/js/2.2825da36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/proton-note/static/js/2.2825da36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d41d322dd648297e007",
+    "url": "/proton-note/static/js/main.e5000a57.chunk.js"
+  },
+  {
+    "revision": "6fc82a00a5ebfac6cb36",
+    "url": "/proton-note/static/js/runtime-main.c951ceaa.js"
+  },
+  {
+    "revision": "243f40f0f8a48be6efb09c5d96804ee7",
+    "url": "/proton-note/static/media/loading.243f40f0.svg"
+  }
+]);
